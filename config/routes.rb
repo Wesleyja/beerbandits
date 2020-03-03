692d47ff2b5e8a2423df785098030db80ad5ff1b
Wesley Fletcher <wes.j.fletcher@gmail.com>
2020-03-03 13:33:08 +1100

    routes for main application

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'preferences', to: 'pages#preferences'
+  get 'preferences/results', to: 'pages#results'
+  get 'about', to: 'pages#about'
 end
