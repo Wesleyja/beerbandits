2d253c7a9463b61a7917c60b23380c8b7531dbde
Lizhenan Li <lizhenanl54@gmail.com>
2020-03-03 14:42:00 +1100

    start with the whole flow

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'preferences', to: 'pages#preferences'
-  get 'preferences/results', to: 'pages#results'
+  get 'preferences', to: 'pages#preferences', as: :preferences
+  get 'preferences/results', to: 'pages#results', as: :results
   get 'about', to: 'pages#about'
 end
