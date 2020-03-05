0bf8d7c6105c5bacc425c7e61c88ea6d0a36b9f7
Wesley Fletcher <wes.j.fletcher@gmail.com>
2020-03-05 16:17:52 +1100

    search toward map for first screen

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,18 +2,29 @@ class SearchController < ApplicationController
   skip_before_action :authenticate_user!
 
   def preferences
+
   end
 
-  def results
+  def results()
     # raise
-    @stores = Store.all
-    @markers = find_stores(@stores)
+    results = InventoryProduct.all.select{ |inventory_product| inventory_product.product.size == 6 && inventory_product.product.drink.category == "Lager" }
+    final_results = {}
+    stores = Store.all.near(params[:location], 2)
+    results.each do |result|
+      if stores.include?(result.inventory.store)
+        price = result.inventory.price_cents.to_f/100
+        distance = Store.find(result.inventory.store_id).distance_to(Geocoder.search(params[:location]).first.coordinates) * 1000
+        ranked_value = ((590.4761905/5)*price)/distance
+        final_results[result] = ranked_value
+      end
+    end
+    final_results = final_results.sort_by {|k, v| [v, k]}
+    @markers = find_stores(stores)
   end
 
   private
 
-  def find_stores(parameters)
-    stores = parameters.near(params[:location], 2)
+  def find_stores(stores)
     @markers = stores.map do |store|
       {
         lat: store.latitude,
