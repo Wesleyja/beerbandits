e2357e1aea2f1b9cb0df340285df69af5401dc2f
Wesley Fletcher <wes.j.fletcher@gmail.com>
2020-03-08 12:38:29 +1100

    another change to make seeds work

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,17 +38,9 @@ puts "Making Users"
   puts "#{User.last.email}"
 end
 
-urls = ["lat=-37.813907&lon=144.96324",
- "lat=-37.904751&lon=145.162538",
- "lat=-37.777210&lon=145.120348",
- "lat=-37.724354&lon=144.990448",
- "lat=-37.748195&lon=144.871142",
- "lat=-37.819827&lon=144.795136"
-]
+
 urls.each_with_index do |latlong, index|
-  url = "https://www.liquorland.com.au/api/FindClosest/ll?#{latlong}"
-  sleep(rand(100..200))
-  file = URI.open(url, "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:73.0) Gecko/20100101 Firefox/73.0").read
+  file = URI.open("https://www.liquorland.com.au/api/FindClosest/ll?lat=-37.819827&lon=144.795136", "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:73.0) Gecko/20100101 Firefox/73.0").read
   data_hash = JSON.parse(file)
   data_hash.each do |store|
     if Store.find_by(latitude: store["latitude"].to_f).nil?
@@ -66,9 +58,17 @@ data_hash["Stores"].each do |store|
     puts "#{Store.last.name} created at Latitude: #{Store.last.latitude}, Longitude: #{Store.last.longitude}"
   end
 end
+urls = ["lat=-37.813907&lon=144.96324",
+ "lat=-37.904751&lon=145.162538",
+ "lat=-37.777210&lon=145.120348",
+ "lat=-37.724354&lon=144.990448",
+ "lat=-37.748195&lon=144.871142",
+]
 urls.each_with_index do |latlong, index|
   url = "https://www.firstchoiceliquor.com.au/api/FindClosest/fc?#{latlong}"
-  sleep(rand(100..200))
+  time_gate = rand(150..200)
+  puts "sleeping for #{time_gate} seconds"
+  sleep(time_gate)
   file = URI.open(url, "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:73.0) Gecko/20100101 Firefox/73.0").read
   data_hash = JSON.parse(file)
   data_hash.each do |store|
