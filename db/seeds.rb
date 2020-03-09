3c330500db684b549ec89663d982f0d552183513
Wesley Fletcher <wes.j.fletcher@gmail.com>
2020-03-09 14:23:13 +1100

    hard coding the information

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,62 +38,40 @@ puts "Making Users"
   User.create(username: Faker::Internet.username,password: 'password', email: Faker::Internet.email)
   puts "#{User.last.email}"
 end
-file_paleale = File.read(Rails.root.join('lib', 'seeds', 'bws-pale-ale.json'))
-file_lager = File.read(Rails.root.join('lib', 'seeds', 'bws-lager.json'))
-file_ipa = File.read(Rails.root.join('lib', 'seeds', 'bws-ipa.json'))
-file_cider = File.read(Rails.root.join('lib', 'seeds', 'bws-cider.json'))
-bws_jsons = [file_lager, file_ipa, file_paleale, file_cider]
-categories = ["Lager", "IPA", "Pale Ale", "Cider"]
-bws_jsons.each_with_index do |url, index|
-  data_hash = JSON.parse(url)
-  data_hash["Bundles"].each do |beer|
-    beer["Products"].each do |x|
-    # if the value is true, then its the hash with the single unit bottle, which has the % and Brand and Volume
-      if x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "productunitquantity" }]["Value"] == "1"
-        if x["AdditionalDetails"].find_index { |i| i["Name"] == "liquorsize" }.nil?
-          volume = x["PackageSize"].gsub(/\D/, "").to_i || 0
-        else
-          volume = x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "liquorsize" }]["Value"].gsub(/\D/, "").to_i
-        end
-        if x["AdditionalDetails"].find_index { |i| i["Name"] == "alcohol%" }.nil?
-          abv = 0
-        elsif x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "alcohol%" }]["Value"].gsub(/\D$/, "").to_f > 12.0
-          x["AdditionalDetails"].find_index { |i| i["Name"] == "standarddrinks" }
-        else
-          abv = x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "alcohol%" }]["Value"].gsub(/\D$/, "").to_f
-        end
-        Drink.create(
-        name: x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "product_short_name" }]["Value"],
-        category: "#{categories[index]}",
-        volume: volume,
-        abv: abv
-        )
-        puts "Drink = #{Drink.last.name}"
-        # Category = x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "liquorstyle" }]["Value"]
-        # Brand = x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "brand_name" }]["Value"]
-      end
-    end
-
-    puts "Making Products"
-
-    beer["Products"].each do |x|
-      # if Drink.find_by(product_number: #value ).nil? -> then add new product / otherwise update
-      if x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "productunitquantity" }]["Value"].empty?
-        size = x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "displayunitquantity" }]["Value"].to_i
-      else
-        size = x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "productunitquantity" }]["Value"].to_i
-      end
-      if x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "webpacktype" }]["Value"] == "Case" && x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "productunitquantity" }]["Value"] == "1"
-        size = x["AdditionalDetails"][x["AdditionalDetails"].find_index { |i| i["Name"] == "displayunitquantity" }]["Value"].to_i
-      end
-      Product.create(drink_id: Drink.last.id, size: size)
-      stores = Store.where(brand_id: Brand.find_by(name: 'BWS')).ids
-      stores.each do |id|
-        Inventory.create(price: x["Price"], store_id: id)
-        InventoryProduct.create(inventory_id: Inventory.last.id, product_id: Product.last.id)
-        puts "#{Product.last.size} pack of #{Drink.last.name} for $#{(Inventory.last.price)} at #{Store.find( Inventory.last.store_id).name}"
-      end
-    end
+postcodes = ["3121", "3141", "3142", "3122", "3101", "3068", "3066", "3067", "3002" ]
+file = File.read(Rails.root.join('lib', 'seeds', 'll-locations.json'))
+data_hash = JSON.parse(file)
+data_hash.each do |store|
+  if Store.find_by(latitude: store["latitude"].to_f).nil? && postcodes.include?(store["postcode"])
+    Store.create(latitude: store["latitude"].to_f,longitude: store["longitude"].to_f, brand_id: Brand.find_by(name: "Liqourland").id, name: "Liqourland #{store["storeName"]}" )
+    puts "#{Store.last.name} created at Latitude: #{Store.last.latitude}, Longitude: #{Store.last.longitude}"
+  end
+end
+file = File.read(Rails.root.join('lib', 'seeds', 'fc-locations.json'))
+data_hash = JSON.parse(file)
+data_hash.each do |store|
+  if Store.find_by(latitude: store["latitude"].to_f).nil? && postcodes.include?(store["postcode"])
+    Store.create(latitude: store["latitude"].to_f,longitude: store["longitude"].to_f, brand_id: Brand.find_by(name: "First Choice").id, name: "First Choice #{store["storeName"]}" )
+    puts "#{Store.last.name} created at Latitude: #{Store.last.latitude}, Longitude: #{Store.last.longitude}"
+    postcodes << store["postcode"]
+  end
+end
+file = File.read(Rails.root.join('lib', 'seeds', 'bws-locations.json'))
+data_hash = JSON.parse(file)
+data_hash["Stores"].each do |store|
+  if postcodes.include?(store["Postcode"])
+    store_name = store["Name"]
+    Store.create(latitude: store["Latitude"].to_f,longitude: store["Longitude"].to_f, brand_id: Brand.find_by(name: "BWS").id, name: "BWS #{store_name}" )
+    puts "#{Store.last.name} created at Latitude: #{Store.last.latitude}, Longitude: #{Store.last.longitude}"
+  end
+end
+file = File.read(Rails.root.join('lib', 'seeds', 'danmurphys-location.json'))
+data_hash = JSON.parse(file)
+data_hash["Stores"].each do |store|
+  if postcodes.include?(store["Postcode"])
+    store_name = store["Name"]
+    Store.create(latitude: store["Latitude"].to_f,longitude: store["Longitude"].to_f, brand_id: Brand.find_by(name: "Dan Murphy's").id, name: "Dan Murphy's #{store_name}" )
+    puts "#{Store.last.name} created at Latitude: #{Store.last.latitude}, Longitude: #{Store.last.longitude}"
   end
 end
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
