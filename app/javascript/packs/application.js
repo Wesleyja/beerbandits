36330546300b7c811b75b245ad95f50ec05bea9e
cassandra calescu <casscalescu@gmail.com>
2020-03-11 16:57:30 +1100

    added loading page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,10 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initMultistepForm } from '../components/preferences_form';
 import { initModalPopup } from '../components/modal';
-
+// import { initLoadingPage } from '../components/loading_page';
 
 initMapbox();
 initAutocomplete();
 initMultistepForm();
 initModalPopup();
+// initLoadingPage();
